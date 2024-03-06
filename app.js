77bc649e88a5977a910994ad9f258d8844179123
efraim-paiva <efgaroto@gmail.com>
2024-03-06 20:06:23 -0300

    Add reiniciar function to reset input fields and reload page

--- app.js
+++ app.js
@@ -36,4 +36,17 @@ function alterarStatusBotao() {
         botao.classList.remove('container__botao');
         botao.classList.add('container__botao-desabilitado');
     }
+
+    console.log(botao);
 }
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
+    window.location.reload();
+}
+
+
