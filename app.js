b66c973cdcd6aabe0aca39310a3050a350174f5a
efraim-paiva <efgaroto@gmail.com>
2024-03-06 18:34:46 -0300

    Add sortear function to app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+function sortear() {
+    var quantidade = document.getElementById('quantidade').value;
+    var de = document.getElementById('de').value;
+    var ate = document.getElementById('ate').value;
+
+    alert(`Quantidade: ${quantidade} De: ${de} Até: ${ate}`);
+}
