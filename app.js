97e9036adcdfb800a108e2452f950921e3440c7e
efraim-paiva <efgaroto@gmail.com>
2024-03-06 19:36:31 -0300

    Add function to alter button status

--- app.js
+++ app.js
@@ -19,8 +19,21 @@ function sortear() {
 
     var resultado = document.getElementById('resultado');
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+
+    alterarStatusBotao();
 }
 
 function numeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao() {
+    var botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
 }
