a770a19a0b71fba60f4d130bb95285d9124ec892
efraim-paiva <efgaroto@gmail.com>
2024-03-06 18:56:31 -0300

    Add random number generator to sortear function

--- app.js
+++ app.js
@@ -3,5 +3,19 @@ function sortear() {
     var de = parseInt(document.getElementById('de').value);
     var ate = parseInt(document.getElementById('ate').value);
 
-    alert(`Quantidade: ${quantidade} De: ${de} Até: ${ate}`);
+
+    var sorteados = [];
+    var numero;
+    for (var i = 0; i < quantidade; i++) {
+        numero = numeroAleatorio(de, ate);
+        sorteados.push(numero);
+    }
+
+    alert(sorteados);
+
+}
+
+function numeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+
 }
